0df5d845323ae2f2580025086c79ad2499a8726a
Dmitrii Girskii <d.girsky@gmail.com>
2025-06-28 14:21:14 +0300

    fix stack

--- components/desk.jsx
+++ components/desk.jsx
@@ -6,10 +6,11 @@ import tarotCards from './tarotCards';
 
 export default function TarotDesk() {
     const numbers = Array.from({ length: 78 }, (_, index) => index + 1);
-    const maxSelectedCards = 5; // Максимальное количество выбранных карт
+    const maxSelectedCards = 3; // Максимальное количество выбранных карт
     // State to store selected cards
     const [selectedCards, setSelectedCards] = useState([]);
     const [animatingCard, setAnimatingCard] = useState(null);
+    const [fullCardInfo, setFullCardInfo] = useState(null)
 
     // Function to handle card click
     const handleCardClick = (card) => {
@@ -24,28 +25,46 @@ export default function TarotDesk() {
         }, 500);
     };
 
-    return (
-        <div className={styles.desk}>
+    const showFullCard = (card) => {
+        setFullCardInfo(card)
+    }
 
+    return (
+  
+            <>
+        {fullCardInfo && (
+            <div className={styles.fullCard} onClick={() => showFullCard(null)}>
+                {/* Render fullCardInfo or other elements */}
+                <div>
+                    <img src={`${import.meta.env.BASE_URL}images/${fullCardInfo.image}`} alt={fullCardInfo.title}/>
+                    <h2>{fullCardInfo.title}</h2>
+                    <p>{fullCardInfo.description}</p>
+                </div>
+            </div>
+        )}
+
+        <div className={styles.desk}>
             <div className={styles.stack_select}>
-                <h2>Select Cards</h2>
+                {/* <h2>Select Cards</h2> */}
                 <ul>
                     {selectedCards.map(card => (
-                        <li key={card}>
+                        <li
+                            key={card}
+                            onClick={() => showFullCard(card)}>
                             <div className={styles.card_false}>
                                 <figure>
                                     <img src={`${import.meta.env.BASE_URL}images/${card.image}`} alt={card.title}/>
                                 </figure>
                             </div>
-                            <p>{card.title}</p>
-                            <p>{card.description}</p>
+                            {/* <p>{card.title}</p> */}
+                            {/* <p>{card.description}</p> */}
                         </li>
                     ))}
 
                     {/* Добавление пустых ячеек, если карт меньше 9 */}
                     {Array.from({ length: maxSelectedCards - selectedCards.length }).map((_, index) => (
                         <li key={`empty-${index}`} className={styles.emptyCard}>
-                            <p>Empty slot</p>
+                            {/* <p>Empty slot</p> */}
                         </li>
                     ))}
 
@@ -54,13 +73,13 @@ export default function TarotDesk() {
 
             <div style={
                 {
-                    overflow: 'scroll',
-                    position: 'fixed',
+                    // overflow: 'scroll',
+                    // position: 'fixed',
                     left: '0px',
                     right: '0px',
                     bottom: '0px',
                     padding: '0 20px 20px',
-                    background: 'linear-gradient(transparent, black)'
+                    // background: 'linear-gradient(transparent, black)'
                 }
             }>
                 <div className={styles.stack}>
@@ -76,5 +95,6 @@ export default function TarotDesk() {
                 </div>
             </div>
         </div>
+        </>
     )
 }
